8c944f1c22ece53cfe9852d693dcb188cd24319e
dem4ply <dem4ply@gmail.com>
2019-03-08 16:21:06 -0600

    add tests for the responses

--- setup.py
+++ setup.py
@@ -9,17 +9,19 @@ README = open(os.path.join( here, 'README.rst' ) ).read()
 
 setup(
     name='mudskipper',
-    version='1.1',
+    version='1.2',
     description='',
     long_description=README,
     license='',
     author='',
     author_email='',
     packages=[ 'mudskipper' ],
-    install_requires=['requests<=2.19.1', 'zeep==2.4.0'],
+    install_requires=[
+        'requests<=2.19.1', 'zeep>=2.4.0',
+        'xmltodict>=0.10.0' ],
     dependency_links=[],
     classifiers=[
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python',
-    ])
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ] )
